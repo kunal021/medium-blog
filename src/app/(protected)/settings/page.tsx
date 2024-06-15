b4e9f8c26046a53ce5c2f26d4b7bed2b34fedfdb
Kunal Singh <ks820177@gmail.com>
2024-06-15 17:40:32 +0530

    update

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,7 +3,7 @@ import UserSignOut from "@/components/settings/UserSignOut";
 
 function page() {
   return (
-    <div className="flex flex-col-reverse justify-center items-center md:flex-row md:justify-between md:items-start m-5">
+    <div className="flex flex-col-reverse justify-center items-center md:flex-row md:justify-between md:items-start m-2 md:m-5">
       <div className="md:w-[70%]">
         <UserPosts />
       </div>
