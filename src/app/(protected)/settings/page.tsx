57b17e870350e1bd42bc6b8cf12925fe67320579
Kunal Singh <ks820177@gmail.com>
2024-06-16 15:37:40 +0530

    update

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,11 +3,11 @@ import UserSignOut from "@/components/settings/UserSignOut";
 
 function page() {
   return (
-    <div className="flex flex-col-reverse justify-center w-full items-center md:flex-row md:justify-between md:items-start my-5 md:mx-5">
+    <div className="flex flex-col-reverse justify-center w-full items-center md:flex-row md:justify-between md:items-start my-5">
       <div className="w-full md:w-[70%]">
         <UserPosts />
       </div>
-      <div className="w-[70%] md:w-[25%] border-2 rounded-md border-black p-5">
+      <div className="w-[70%] md:w-[25%] border-2 rounded-md border-black p-5 mx-10">
         <UserSignOut />
       </div>
     </div>
